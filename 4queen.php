fdb60715df26a519fb6c49341f1cf1a3cbb75c4a
daigakuimo <enaena128@outlook.jp>
2019-05-31 00:01:42 +0900

    初めてのコミット

---
+++ 4queen.php
@@ -0,0 +1,64 @@
+<?php
+
+    $ga = new geneticAlgorithm();
+
+    $ga->init();
+    $ga->main();
+
+    //model
+    class fourQueen{
+        public $position;
+        public $fitness;
+
+        public function __construct($q1,$q2,$q3,$q4){
+            $position = array($q1,$q2,$q3,$q4);
+        }
+
+    }
+
+    //controller
+    class geneticAlgorithm{
+
+        public function main(){
+
+            do{
+
+                $this::selection();
+
+                $this::crossover();
+
+                $this::mutation();
+
+                $this::evaluation();
+
+            }while(terminationCondition());
+
+        }
+
+        public function init(){
+
+        }
+
+        public function selection(){
+
+        }
+
+        public function crossover(){
+
+        }
+
+        public function mutation(){
+
+        }
+
+        public function evaluation(){
+
+        }
+
+        public function terminationCondition(){
+
+        }
+
+    }
+
+?>
